f43fca62a60b33f4c1ba7638a4660ab3a9efa70f
Ryan Dupont <ryanandrewdupont@gmail.com>
2025-04-17 16:44:07 -0400

    Network info

--- screens/Main.js
+++ screens/Main.js
@@ -1,8 +1,14 @@
-import { Image, Platform, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ToastAndroid,
+} from "react-native";
 import Constants from "expo-constants";
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import DirectoryScreen from "./DirectoryScreen";
-import FavoritesScreen from "./FavoritesScreen";
 import { createStackNavigator } from "@react-navigation/stack";
 import {
   createDrawerNavigator,
@@ -13,7 +19,6 @@ import HomeScreen from "./HomeScreen";
 import AboutScreen from "./AboutScreen";
 import ContactScreen from "./ContactScreen";
 import ReservationScreen from "./ReservationScreen";
-import LoginScreen from './LoginScreen';
 import { Icon } from "react-native-elements";
 import logo from "../assets/images/logo.png";
 import { useDispatch } from "react-redux";
@@ -22,6 +27,10 @@ import { fetchPartners } from "../features/partners/partnersSlice";
 import { fetchCampsites } from "../features/campsites/campsitesSlice";
 import { fetchPromotions } from "../features/promotions/promotionsSlice";
 import { fetchComments } from "../features/comments/commentsSlice";
+import FavoritesScreen from "./FavoritesScreen";
+import LoginScreen from "./LoginScreen";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/core";
+import NetInfo from "@react-native-community/netinfo";
 
 const Drawer = createDrawerNavigator();
 
@@ -127,45 +136,50 @@ const ReservationNavigator = () => {
 const FavoritesNavigator = () => {
   const Stack = createStackNavigator();
   return (
-      <Stack.Navigator screenOptions={screenOptions}>
-          <Stack.Screen
-              name='Favorites'
-              component={FavoritesScreen}
-              options={({ navigation }) => ({
-                  title: 'Favorite Campsites',
-                  headerLeft: () => (
-                      <Icon
-                          name='heart'
-                          type='font-awesome'
-                          iconStyle={styles.stackIcon}
-                          onPress={() => navigation.toggleDrawer()}
-                      />
-                  )
-              })}
-          />
-      </Stack.Navigator>
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={({ navigation }) => ({
+          title: "Favorite Campsites",
+          headerLeft: () => (
+            <Icon
+              name="heart"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
   );
 };
 
 const LoginNavigator = () => {
   const Stack = createStackNavigator();
   return (
-      <Stack.Navigator screenOptions={screenOptions}>
-          <Stack.Screen
-              name='Login'
-              component={LoginScreen}
-              options={({ navigation }) => ({
-                  headerLeft: () => (
-                      <Icon
-                          name='sign-in'
-                          type='font-awesome'
-                          iconStyle={styles.stackIcon}
-                          onPress={() => navigation.toggleDrawer()}
-                      />
-                  )
-              })}
-          />
-      </Stack.Navigator>
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={({ navigation, route }) => ({
+          headerTitle: getFocusedRouteNameFromRoute(route),
+          headerLeft: () => (
+            <Icon
+              name={
+                getFocusedRouteNameFromRoute(route) === "Register"
+                  ? "user-plus"
+                  : "sign-in"
+              }
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
   );
 };
 
@@ -223,6 +237,44 @@ const Main = () => {
     dispatch(fetchComments());
   }, [dispatch]);
 
+  useEffect(() => {
+    NetInfo.fetch().then((connectionInfo) => {
+      Platform.OS === "ios"
+        ? Alert.alert("Initial Network Connectivity Type:", connectionInfo.type)
+        : ToastAndroid.show(
+            "Initial Network Connectivity Type: " + connectionInfo.type,
+            ToastAndroid.LONG
+          );
+    });
+
+    const unsubscribeNetInfo = NetInfo.addEventListener((connectionInfo) => {
+      handleConnectivityChange(connectionInfo);
+    });
+
+    return unsubscribeNetInfo;
+  }, []);
+
+  const handleConnectivityChange = (connectionInfo) => {
+    let connectionMsg = "You are now connected to an active network.";
+    switch (connectionInfo.type) {
+      case "none":
+        connectionMsg = "No network connection is active.";
+        break;
+      case "unknown":
+        connectionMsg = "The network connection state is now unknown.";
+        break;
+      case "cellular":
+        connectionMsg = "You are now connected to a cellular network.";
+        break;
+      case "wifi":
+        connectionMsg = "You are now connected to a WiFi network.";
+        break;
+    }
+    Platform.OS === "ios"
+      ? Alert.alert("Connection change:", connectionMsg)
+      : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
+  };
+
   return (
     <View
       style={{
@@ -239,19 +291,19 @@ const Main = () => {
         }}
       >
         <Drawer.Screen
-          name='Login'
+          name="Login"
           component={LoginNavigator}
           options={{
-              headerShown: false,
-              drawerIcon: ({ color }) => (
-                  <Icon
-                      name='sign-in'
-                      type='font-awesome'
-                      size={24}
-                      iconStyle={{ width: 24 }}
-                      color={color}
-                  />
-              )
+            headerShown: false,
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="sign-in"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
           }}
         />
         <Drawer.Screen
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,3 +1,2 @@
 // export const baseUrl = 'http://192.168.12.27:3001/'; //HOME
-export const baseUrl = 'http://10.21.101.88:3001/'; //WORK 
-// export const baseUrl = 'http://10.21.101.61:3001/'; //WORK2
+export const baseUrl = 'http://10.21.101.61:3001/'; //WORK 
